c57f48f7a8d9bab7170e9c2a4a823b15ab7a5bf3
Ziga Dolar <ziga@upalela.com>
2015-05-18 18:07:35 +0200

    Added support for attributed string

--- BDKNotifyHUD/BDKNotifyHUD.h
+++ BDKNotifyHUD/BDKNotifyHUD.h
@@ -14,12 +14,19 @@
 
 @property (strong, nonatomic) UIColor *borderColor;
 @property (strong, nonatomic) NSString *text;
+@property (strong, nonatomic) NSAttributedString *attributedText;
 
 + (id)notifyHUDWithView:(UIView *)view text:(NSString *)text;
 + (id)notifyHUDWithImage:(UIImage *)image text:(NSString *)text;
 
++ (id)notifyHUDWithView:(UIView *)view attributedText:(NSAttributedString *)attributedText;
++ (id)notifyHUDWithImage:(UIImage *)image attributedText:(NSAttributedString *)attributedText;
+
 - (id)initWithView:(UIView *)view text:(NSString *)text;
 - (id)initWithImage:(UIImage *)image text:(NSString *)text;
+
+- (id)initWithImage:(UIImage *)image attributedText:(NSAttributedString *)attributedText;
+- (id)initWithView:(UIView *)view attributedText:(NSAttributedString *)attributedText;
 
 - (void)setImage:(UIImage *)image;
 - (void)presentWithDuration:(CGFloat)duration speed:(CGFloat)speed inView:(UIView *)view completion:(void (^)(void))completion;
